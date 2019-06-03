4c6b7c942a744a1ba9962eac6375ed27d71fc71b
abaanahmed <51101449+abaanahmed@users.noreply.github.com>
2019-06-02 20:15:16 -0700

    added comments and extremely minor changes

--- WumpusTest/Player.cs
+++ WumpusTest/Player.cs
@@ -13,55 +13,55 @@ namespace WumpusTest
         private int arrow;
         private int gold;
         private int roomNumber;
-
+        // constructor
         public Player()
         {
-            gold = 0;
+            gold = 100;
             arrow = 0;
             Random RNG = new Random();
             roomNumber = getValidRoomNumber(RNG);
         }
-
+        // postcondition: a random number between 1-30 inclusive is returned
         private int getValidRoomNumber(Random RNG)
         {
             return RNG.Next(29) + 1;
         }
-
+        // this allows gold amount to be changed
         public void setGold(int k)
         {
             gold = k;
         }
-
-        public void addOneGold()
+        // often gold is being reduced by one so this method is for convenience
+        public void spendOneGold()
         {
-            gold++;
+            gold--;
         }
-
+        // often arrows are being increased by one so this method is for convenience
         public void addOneArrow()
         {
             arrow++;
         }
-
+        // often arrows are being decreased by one so this method is for convenience
         public void SpendAnArrow()
         {
             arrow--;
         }
-
+        // simple accessor
         public int getArrows()
         {
             return arrow;
         }
-
+        // simple accessor
         public int getGold()
         {
             return gold;
         }
-
+        // simple mutator
         public int getRoomNumber()
         {
             return roomNumber;
         }
-
+        // a method gc can call to see if the game needs to be ended instantly
         public bool CanCanContinuePlaying()
         {
             if (arrow < 0 || gold < 0)
@@ -70,7 +70,7 @@ namespace WumpusTest
             }
             return true;
         }
-
+        // simple mutator that checks if a valid roomNumber was passed
         public void setRoomNumber(int k)
         {
             if (k <= 30)
