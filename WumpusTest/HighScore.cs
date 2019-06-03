49ae556e4a87642c4b95a158f4b6be185f4717a8
abaanahmed <51101449+abaanahmed@users.noreply.github.com>
2019-06-03 07:09:40 -0700

    Update HighScore.cs

--- WumpusTest/HighScore.cs
+++ WumpusTest/HighScore.cs
@@ -1,4 +1,6 @@
-﻿using System;
+
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,43 +8,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WumpusTest
 {
     public class HighScore
     {
         // instance variables
         private int[] scores = new int[10];
-        private string[] scoresAsString = new string[10];
+        private String[] scoresAsString = new String[10];
         private List<string> scoresAsArrayList = new List<string>();
-        private string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\HighScores.txt";
+        private string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\HighScores.txt"; 
 
-        public HighScore()
+        // constructor loads in scores from a file
+        public HighScore() 
         {
             scoresAsString = File.ReadAllLines(filePath);
-            string[] str;
-            int score;
-            for (int i = 0; i < scoresAsString.Length; i++)
-            {
-                str = scoresAsString[i].Split(new char[] { ' ' });
-                score = Int32.Parse(str[1]);
-                scores[i] = score;
-            }
-            scoresAsArrayList.AddRange(scoresAsString);
+            scoresAsArrayList.AddRange(scoresAsString); 
         }
-
-        public void displayHighScores()
+        // precondition: Scores have been initialized and name is a valid string and newScore is a valid int
+        // postcondition:A score is added to the field scoresAsArrayList
+        public void addScore(String name, int newScore)
         {
-            for (int i = scoresAsArrayList.Count - 1; i >= 0; i--)
-            {
-                Console.WriteLine(scoresAsArrayList[i]);
+            if (checkForHighScore(newScore))
+            { // if list is full remove lowst number
+                if (scoresAsArrayList.Count() >= 10)
+                {
+                    scoresAsArrayList.RemoveAt(0);
+                }
+                int scoreAsInt;
+                String scoreAsString;
+                for (int i = 0; i < scoresAsArrayList.Count; i++)
+                {
+                    String[] str = scoresAsArrayList.ElementAt(i).Split(new char[] { ' ' });
+                    scoreAsString = str[1];
+                    scoreAsInt = Int32.Parse(scoreAsString);
+                    if (newScore < scoreAsInt)
+                    {
+                        scoresAsArrayList.Insert(i, name + " " + newScore.ToString());
+                        break;
+                    }
+                }
+                writeToFile();
             }
         }
-
-        public bool checkForHighScore(int newScore)
+        // checks if the new score is withing the top 10 scores
+        private Boolean checkForHighScore(int newScore)
         {
             if (!isListFull())
             {
-                return true; 
+                return true;
             }
             else
             {
@@ -50,51 +64,61 @@ namespace WumpusTest
                 {
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
         }
-
-        public void rearrangeScores(string name, int newScore)
+        // checks if highscore has too many values
+        private Boolean isListFull()
         {
-            if (scoresAsArrayList.Count() >= 10)
-            {
-                scoresAsArrayList.RemoveAt(0);
-            }
-
-            for (int i = 0; i < scoresAsArrayList.Count; i++)
-            {
-
-                if(newScore < scores[i])
-                {
-                    scoresAsArrayList.Insert(i, name + " " + newScore.ToString());
-                    break;
-                }
-            }
-
-        }
-
-        public void writeToFile()
-        {
-
-            File.WriteAllLines(filePath, scoresAsArrayList);
-            
-        }
-
-        public bool isListFull()
-        {
-            if(scores.Length >= 10)
+            if (scores.Length >= 10)
             {
                 return true;
             }
-            else
+            return false;
+        }
+        // method to update the highscore file
+        private void writeToFile()
+        {
+            File.WriteAllLines(filePath, scoresAsArrayList);
+        }
+        // passes just the names of high scorers
+        public string[] getNames()
+        {
+            string[] arrayScores = scoresAsArrayList.ToArray();
+            string[] splitString;
+            string[] names = setStartingState();
+            for (int i = 0; i < arrayScores.Length; i++)
             {
-                return false;
+                splitString = arrayScores[i].Split(null);
+                names[i] = splitString[0];
             }
+            return names;
+        }
+        // passes the scores as a string array        
+        public string[] getScores()
+        {
+            string[] splitString;
+            string[] scores = setStartingState();
+            for (int i = 0; i < scoresAsArrayList.Count; i++)
+            {
+                splitString = scoresAsArrayList.ElementAt(i).Split(' ');
+                scores[i] = splitString[1];
+            }
+            return scores;
+        }
+        
+        // propagates the string array with null values
+        private string[] setStartingState()
+        {
+            string[] arr = new string[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = "None";
+            }
+            return arr;
         }
 
     }
 
 }
+
